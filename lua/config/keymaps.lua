07659c398adecf81f8b02e6a525fb8152c71b1b3
MerlinSink <MerlinSinkZero@outlook.com>
2024-08-28 22:30:14 +0800

    构建neovim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,56 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+
+-- Nohlsearch
+keymap("n", "<LEADER><CR>", ":nohlsearch<CR>", opts)
+-- Reload init.lua
+-- keymap("n", "<C-n>", ":source $MYVIMRC<CR>", opts)
+
+-- Save & quit
+keymap("n", "Q", ":q<CR>", opts)
+-- noremap <C-q> :qa<CR>
+keymap("n", "W", ":w<CR>", opts)
+
+-- Move Normal
+keymap("n", "J", "5j", opts)
+keymap("n", "K", "5k", opts)
+keymap("n", "H", "5h", opts)
+keymap("n", "L", "5l", opts)
+-- Move Visual
+keymap("v", "J", "5j", opts)
+keymap("v", "K", "5k", opts)
+keymap("v", "H", "5h", opts)
+keymap("v", "L", "5l", opts)
+
+-- Open lazygit
+keymap("n", "<C-g>", ":tabe<CR>:-tabmove<CR>:term lazygit<CR> i", opts)
+
+-- Split
+keymap("n", "<Space>sl", ":set splitright<CR>:vsplit<CR>", opts)
+keymap("n", "<Space>sh", ":set nosplitright<CR>:vsplit<CR>", opts)
+keymap("n", "<Space>sj", ":set splitbelow<CR>:split<CR>", opts)
+keymap("n", "<Space>sk", ":set nosplitbelow<CR>:split<CR>", opts)
+
+-- Change Split
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "su", "<C-w>t<C-w>H", opts)
+keymap("n", "si", "<C-w>t<C-w>K", opts)
+
+-- Change Size
+keymap("n", "<UP>", ":resize +5<CR>", opts)
+keymap("n", "<DOWN>", ":resize -5<CR>", opts)
+keymap("n", "<LEFT>", ":vertical resize +5<CR>", opts)
+keymap("n", "<RIGHT>", ":vertical resize -5<CR>", opts)
+
+-- Create a tab and move
+keymap("n", "tj", ":tabe<CR>", opts)
+-- map("n", "th", ":-tabnext<CR>", opts)
+-- map("n", "tl", ":+tabnext<CR>", opts)
+--
+-- lazy.nvim
+keymap("n", "<leader>ll", "<cmd>Lazy<Cr>", opts)

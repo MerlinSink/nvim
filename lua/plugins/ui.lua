07afec86e056cb95405fbb045c5272028c26156c
MerlinSink <MerlinSinkZero@outlook.com>
2025-04-02 11:05:40 +0800

    删除indent-blankline插件

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -133,36 +133,6 @@ return {
     end,
   },
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "VeryLazy",
-  opts = function()
-    return {
-      indent = {
-        char = "│",
-        tab_char = "│",
-      },
-      scope = { show_start = false, show_end = false },
-      exclude = {
-        filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-      },
-    }
-  end,
-  main = "ibl",
-},
-
 {
   "rcarriga/nvim-notify",
   keys = {

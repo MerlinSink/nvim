d22ffdd900d27f991ad57d9c5768051de58b84a8
MerlinSink <MerlinSinkZero@outlook.com>
2025-04-11 08:36:56 +0800

    更新LSP配置

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -1,159 +1,160 @@
 return {
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		build = ":MasonUpdate",
+		opts_extend = { "ensure_installed" },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shfmt",
+				"lua-language-server",
+				"clangd",
+				"clang-format",
+			},
+			ui = {
+				border = "double",
+				width = 0.7,
+				height = 0.7,
+			},
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local registry = require("mason-registry")
+			registry:on("package:install:success", function()
+				vim.defer_fn(function()
+					-- trigger FileType event to possibly load this newly installed LSP server
+					require("lazy.core.handler.event").trigger({
+						event = "VeryLazy",
+						buf = vim.api.nvim_get_current_buf(),
+					})
+				end, 100)
+			end)
 
-  {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  build = ":MasonUpdate",
-  opts_extend = { "ensure_installed" },
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shfmt",
-      "clangd",
-    },
-    ui = {
-      border = "single",
-      width = 0.7,
-      height = 0.7,
-    },
-  },
-  ---@param opts MasonSettings | {ensure_installed: string[]}
-  config = function(_, opts)
-    require("mason").setup(opts)
-    local mr = require("mason-registry")
-    mr:on("package:install:success", function()
-      vim.defer_fn(function()
-        -- trigger FileType event to possibly load this newly installed LSP server
-        require("lazy.core.handler.event").trigger({
-          event = "FileType",
-          buf = vim.api.nvim_get_current_buf(),
-        })
-      end, 100)
-    end)
+			registry.refresh(function()
+				for _, tool in ipairs(opts.ensure_installed) do
+					local p = registry.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end)
+		end,
+	},
 
-    mr.refresh(function()
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end)
-  end,
-},
+	{
+		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		dependencies = {
+			{ "williamboman/mason-lspconfig.nvim", config = function() end },
+		},
+		opts = {
+			diagnostics = {
+				underline = true,
+				update_in_insert = true,
+				virtual_text = {
+					spacing = 4,
+					source = "if_many",
+					prefix = "●",
+					-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+					-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+					-- prefix = "icons",
+				},
+				virtual_line = false,
+				severity_sort = true,
+			},
+			servers = {
+				lua_ls = {
+					enabled = true,
+					settings = {
+						Lua = {},
+					},
+				},
+				clangd = {
+					enabled = true,
+					cmd = {
+						"clangd",
+					},
+					root_markers = { ".clangd", "compile_commands.json" },
+					filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+				},
+			},
+		},
+		config = function(_, opts)
+			local icons = require("config.icons").diagnostics
+			opts.diagnostics = vim.tbl_deep_extend("force", {
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = icons.Error,
+						[vim.diagnostic.severity.WARN] = icons.Warn,
+						[vim.diagnostic.severity.HINT] = icons.Hint,
+						[vim.diagnostic.severity.INFO] = icons.Info,
+					},
+				},
+			}, opts.diagnostics or {})
 
-  {
-  "neovim/nvim-lspconfig",
-  event = "VeryLazy",
-  dependencies = {
-    { "williamboman/mason-lspconfig.nvim", config = function() end },
-  },
-  opts = function()
-    ---@class PluginLspOpts
-    local icons = require("config.icons")
-    local ret = {
-      -- options for vim.diagnostic.config()
-      ---@type vim.diagnostic.Opts
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-          -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-          -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-          -- prefix = "icons",
-        },
-        severity_sort = true,
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
-            [vim.diagnostic.severity.WARN] = icons.diagnostics.Warn,
-            [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
-            [vim.diagnostic.severity.INFO] = icons.diagnostics.Info,
-          },
-        },
-      },
-      -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-      -- Be aware that you also will need to properly configure your LSP server to
-      -- provide the inlay hints.
-      inlay_hints = {
-        enabled = true,
-        exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
-      },
-      -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
-      -- Be aware that you also will need to properly configure your LSP server to
-      -- provide the code lenses.
-      codelens = {
-        enabled = false,
-      },
-      -- add any global capabilities here
-      capabilities = {
-        workspace = {
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
-        },
-      },
-      -- options for vim.lsp.buf.format
-      -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overridden when specified
-      format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-      },
-      -- LSP Server Settings
-      ---@type lspconfig.options
-      servers = {
-        lua_ls = {
-          -- mason = false, -- set to false if you don't want this server to be installed with mason
-          -- Use this to add any additional keymaps
-          -- for specific lsp servers
-          -- ---@type LazyKeysSpec[]
-          -- keys = {},
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              codeLens = {
-                enable = true,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-            },
-          },
-        },
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    }
-    return ret
-  end,
-},
+			if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
+				opts.diagnostics.virtual_text.prefix = "●"
+					or function(diagnostic)
+						for d, icon in pairs(icons) do
+							if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+								return icon
+							end
+						end
+					end
+			end
 
+			vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+			local servers = opts.servers
+			local has_blink, blink = pcall(require, "blink.cmp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				has_blink and blink.get_lsp_capabilities() or {},
+				opts.capabilities or {}
+			)
+
+			local function setup(server)
+				local server_opts = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, servers[server] or {})
+				if server_opts.enabled == false then
+					return
+				end
+				require("lspconfig")[server].setup(server_opts)
+			end
+
+			for server, _ in pairs(servers) do
+				setup(server)
+			end
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(mode, keys, func, desc)
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+					end
+
+					map("n", "<leader>cl", function() Snacks.picker.lsp_config() end, "Lsp Info")
+					map("n", "gd", function() Snacks.picker.lsp_definitions() end, "Goto Definition")
+					map("n", "gD", function() Snacks.picker.lsp_declarations() end, "Goto Declaration")
+					map("n", "gr", function() Snacks.picker.lsp_references() end, "References")
+					map("n", "gI", function() Snacks.picker.lsp_implementations() end, "Goto Implementation")
+					map("n", "gy", function() Snacks.picker.lsp_type_definitions() end, "Goto T[y]pe Definition")
+					map("n", "gh", function() return vim.lsp.buf.hover() end, "Hover")
+					map("n", "gk", function() return vim.lsp.buf.signature_help() end, "Signature Help")
+					map("i", "<C-k>", function() return vim.lsp.buf.signature_help() end, "Signature Help")
+					map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+					map("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
+					map({ "n", "t" }, "<C-n>", function() Snacks.words.jump(vim.v.count1) end, "Next Reference")
+					map({ "n", "t" }, "<C-p>", function() Snacks.words.jump(-vim.v.count1) end, "Prev Reference")
+					map("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, "LSP Symbols")
+					map("n", "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, "LSP Workspace Symbols")
+				end,
+			})
+		end,
+	},
 }

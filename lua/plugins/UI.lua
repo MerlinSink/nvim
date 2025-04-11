449d03cbfa1411ce402076f7e75ff005983a1f4f
MerlinSink <MerlinSinkZero@outlook.com>
2025-04-11 21:25:02 +0800

    更新UI设置

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -194,13 +194,11 @@ return {
 	},
 
 	{
-		"MunifTanjim/nui.nvim",
-		lazy = true,
-	},
-
-	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
 		opts = {
 			lsp = {
 				override = {
@@ -223,9 +221,11 @@ return {
 				},
 			},
 			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
 		},
     -- stylua: ignore

a77c3470a5343f182003e62c2af0b4bc01c5541c
MerlinSink <MerlinSinkZero@outlook.com>
2025-04-23 22:10:03 +0800

    添加Markdown相关插件

---
+++ lua/plugins/Markdown.lua
@@ -0,0 +1,70 @@
+return {
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+			vim.fn["mkdp#util#install"]()
+		end,
+		keys = {
+			{
+				"<leader>mp",
+				ft = "markdown",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+		config = function()
+			vim.cmd([[do FileType]])
+		end,
+	},
+
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+		opts = {
+			heading = {
+				enabled = true,
+				render_modes = false,
+				atx = true,
+				setext = true,
+				sign = true,
+				icons = { "󰼏 ", "󰎨 ", "󰼑 ", "󰎲 ", "󰼓 ", "󰎴 " },
+			},
+			code = {
+				sign = false,
+				style = "full",
+				width = "block",
+				left_pad = 2,
+				right_pad = 4,
+			},
+			checkbox = {
+				enabled = true,
+				custom = {
+					important = {
+						raw = "[~]",
+						rendered = "󰓎 ",
+						highlight = "DiagnosticWarn",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+			Snacks.toggle({
+				name = "Render Markdown",
+				get = function()
+					return require("render-markdown.state").enabled
+				end,
+				set = function(enabled)
+					local m = require("render-markdown")
+					if enabled then
+						m.enable()
+					else
+						m.disable()
+					end
+				end,
+			}):map("<leader>um")
+		end,
+	},
+}

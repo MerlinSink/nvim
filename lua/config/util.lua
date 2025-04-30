264b8ce47b08f15d644ce82c914a2c84f22849fe
MerlinSink <MerlinSinkZero@outlook.com>
2025-04-30 13:22:48 +0800

    添加自定义事件LazyFile

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -1,18 +1,27 @@
 local M = {}
 
+-- define events
+local lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
+
 ---@param list table<string>
 ---@return table<string>
 function M.dedup(list)
-  local seen = {}
-  local result = {}
-  for _, i in ipairs(list) do
-    if not seen[i] then
-      table.insert(result, i)
-      seen[i] = true
-    end
-  end
-  return result
+	local seen = {}
+	local result = {}
+	for _, i in ipairs(list) do
+		if not seen[i] then
+			table.insert(result, i)
+			seen[i] = true
+		end
+	end
+	return result
+end
+
+-- Add LazyFile event
+function M.lazy_file()
+	local Event = require("lazy.core.handler.event")
+	Event.mappings.LazyFile = { id = "LazyFile", event = lazy_file_events }
+	Event.mappings["User LazyFile"] = Event.mappings.LazyFile
 end
 
 return M
-

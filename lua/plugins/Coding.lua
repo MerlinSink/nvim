affe047b0afb455ea399feb0cc69ecd0a60ebadb
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-08 20:24:35 +0800

    添加toggle-bool插件

--- lua/plugins/Coding.lua
+++ lua/plugins/Coding.lua
@@ -115,4 +115,34 @@ return {
 			end
 		end,
 	},
+
+	{
+		"gerazov/toggle-bool.nvim",
+		event = "VeryLazy",
+		opts = {
+			mapping = "<leader>tb",
+			additional_toggles = {
+				Yes = "No",
+				On = "Off",
+				["0"] = "1",
+				Enable = "Disable",
+				Enabled = "Disabled",
+				First = "Last",
+				Before = "After",
+				Persistent = "Ephemeral",
+				Internal = "External",
+				Start = "Stop",
+				In = "Out",
+				Open = "Close",
+				Enter = "Exit",
+				Lock = "Unlock",
+				Connect = "Disconnect",
+				Mount = "Dismount",
+				Import = "Export",
+				Ingress = "Egress",
+				Allow = "Deny",
+				All = "None",
+			},
+		},
+	},
 }

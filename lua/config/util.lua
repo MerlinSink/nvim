fcca7abacee9ff2d4613a4887fec3159b28ea3ab
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-14 19:07:42 +0800

    添加新的函数

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -49,4 +49,26 @@ function M.load(prefix, modules)
 	return config
 end
 
+-- check system
+function M.is_win()
+	return vim.uv.os_uname().sysname:find("Windows") ~= nil
+end
+
+M.actions = {
+	-- Native Snippets
+	snippet_forward = function()
+		if vim.snippet.active({ direction = 1 }) then
+			vim.schedule(function()
+				vim.snippet.jump(1)
+			end)
+			return true
+		end
+	end,
+	snippet_stop = function()
+		if vim.snippet then
+			vim.snippet.stop()
+		end
+	end,
+}
+
 return M

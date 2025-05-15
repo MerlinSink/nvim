4e7c0620073642fb82dc527fcee043bc3920f50e
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-15 21:33:16 +0800

    更改快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,19 +2,16 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Nohlsearch
-keymap("n", "<Esc>", ":nohlsearch<CR>", opts)
+keymap("n", "<Esc>", "<Cmd>nohlsearch<CR>", opts)
+
+-- Quit
+keymap("n", "<leader>Q", "<Cmd>q<CR>", opts)
 
 -- Move Visual
 keymap("v", "J", "5j", opts)
 keymap("v", "K", "5k", opts)
 keymap("v", "H", "5h", opts)
 keymap("v", "L", "5l", opts)
-
--- Split
-keymap("n", "<leader>sl", ":set splitright<CR>:vsplit<CR>", opts)
-keymap("n", "<leader>sh", ":set nosplitright<CR>:vsplit<CR>", opts)
-keymap("n", "<leader>sj", ":set splitbelow<CR>:split<CR>", opts)
-keymap("n", "<leader>sk", ":set nosplitbelow<CR>:split<CR>", opts)
 
 -- Change Split
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -25,18 +22,12 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "su", "<C-w>t<C-w>H", opts)
 keymap("n", "si", "<C-w>t<C-w>K", opts)
 
--- Change Size
-keymap("n", "<UP>", ":resize +5<CR>", opts)
-keymap("n", "<DOWN>", ":resize -5<CR>", opts)
-keymap("n", "<LEFT>", ":vertical resize +5<CR>", opts)
-keymap("n", "<RIGHT>", ":vertical resize -5<CR>", opts)
-
 -- Create a tab and move
 keymap("n", "nt", ":tabe<CR>", opts)
 -- map("n", "th", ":-tabnext<CR>", opts)
 -- map("n", "tl", ":+tabnext<CR>", opts)
 
 -- lazy.nvim
-keymap("n", "<leader>ol", "<Cmd>Lazy<Cr>", opts)
+keymap("n", "<leader>lz", "<Cmd>Lazy<Cr>", opts)
 -- Conform
 keymap("n", "<leader>ci", "<cmd>ConformInfo<CR>", opts)

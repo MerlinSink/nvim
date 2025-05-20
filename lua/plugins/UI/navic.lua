f02e137331dd4d42cd8921f2111df03e5e37ea8b
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-20 18:14:36 +0800

    添加navic插件

---
+++ lua/plugins/UI/navic.lua
@@ -0,0 +1,31 @@
+return {
+	"SmiteshP/nvim-navic",
+  event = "VeryLazy",
+	init = function()
+		vim.g.navic_silence = true
+		Util.on_attach(function(client, buffer)
+			if client.supports_method("textDocument/documentSymbol") then
+				require("nvim-navic").attach(client, buffer)
+			end
+		end)
+	end,
+	opts = function()
+		return {
+			icons = icons.kinds,
+			lsp = {
+				auto_attach = false,
+				preference = nil,
+			},
+			highlight = true,
+			separator = " ",
+			depth_limit = 0,
+			depth_limit_indicator = "..",
+			safe_output = true,
+			lazy_update_context = true,
+			click = false,
+			format_text = function(text)
+				return text
+			end,
+		}
+	end,
+}

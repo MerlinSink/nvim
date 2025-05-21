ad4adf52c186447f05630b439e44df30b114b9ce
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-21 20:19:08 +0800

    feat:添加新主题

---
+++ lua/plugins/Colorscheme/gruvbox-material.lua
@@ -0,0 +1,11 @@
+return {
+	"sainnhe/gruvbox-material",
+	lazy = true,
+	config = function()
+		-- Optionally configure and load the colorscheme
+		-- directly inside the plugin declaration.
+    vim.o.background = "dark"
+		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_background = "hard"
+	end,
+}

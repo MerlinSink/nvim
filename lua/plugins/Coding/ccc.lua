5e07ea27cd3def285876a7668cc67122d90aceff
MerlinSink <MerlinSinkZero@outlook.com>
2025-05-24 17:55:42 +0800

    fix:change keymap stlye in which-key

--- lua/plugins/Coding/ccc.lua
+++ lua/plugins/Coding/ccc.lua
@@ -2,7 +2,6 @@ return {
 	"uga-rosa/ccc.nvim",
 	event = "LazyFile",
 	keys = {
-		{ "<leader>uH", "<Cmd>CccHighlighterToggle<CR>", desc = "CccHighlighter Toggle" },
 		{ "<leader>th", "<Cmd>CccConvert<CR>", desc = "CccHighlighter Convert" },
 		{ "<leader>Hp", "<Cmd>CccPick<CR>", desc = "CccHighlighter Pick" },
 	},
@@ -12,4 +11,21 @@ return {
 			lsp = true,
 		},
 	},
+	config = function(_, opts)
+		require("ccc").setup(opts)
+		Snacks.toggle({
+			name = "Ccc Highlighter Toggle",
+			get = function()
+				return require("ccc.highlighter").attached_buffer[vim.api.nvim_get_current_buf()]
+			end,
+			set = function(enabled)
+				local m = require("ccc.highlighter")
+				if enabled then
+          m:enable(0)
+				else
+					m:disable(0)
+				end
+			end,
+		}):map("<leader>uH")
+	end,
 }

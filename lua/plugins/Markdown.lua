9cccd86f890ca8e7bc8d790cdd654b7d0bc9be7b
MerlinSink <MerlinSinkZero@outlook.com>
2025-06-29 20:23:37 +0800

    feat: add vim-table-mode

--- lua/plugins/Markdown.lua
+++ lua/plugins/Markdown.lua
@@ -67,4 +67,21 @@ return {
 			}):map("<leader>um")
 		end,
 	},
+
+	{
+		"dhruvasagar/vim-table-mode",
+		ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+		config = function()
+			vim.cmd(
+				[[
+        augroup markdown_config
+          autocmd!
+          autocmd FileType markdown nnoremap <buffer> <M-s> :TableModeRealign<CR>
+        augroup END
+      ]],
+				false
+			)
+			vim.g.table_mode_sort_map = "<leader>mts"
+		end,
+	},
 }

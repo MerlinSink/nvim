f02fb46eb075110adef757d7f89e333a0aafb3f7
MerlinSink <MerlinSinkZero@outlook.com>
2025-06-30 12:13:03 +0800

    fix: 删除快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,12 +10,6 @@ keymap("n", "<leader>W", "<Cmd>w<CR>", opts)
 -- Quit
 keymap("n", "<leader>Q", "<Cmd>q<CR>", opts)
 
--- Move Visual
-keymap("v", "J", "5j", opts)
-keymap("v", "K", "5k", opts)
-keymap("v", "H", "5h", opts)
-keymap("v", "L", "5l", opts)
-
 -- Change Split
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)

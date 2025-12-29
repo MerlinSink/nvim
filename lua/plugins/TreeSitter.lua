a2ea7375d474eb16d2476aaced1e8a07f8643245
MerlinSink <MerlinSinkZero@outlook.com>
2025-12-29 16:09:06 +0800

    fix: fix a bug

--- lua/plugins/TreeSitter.lua
+++ lua/plugins/TreeSitter.lua
@@ -1,37 +1,32 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		version = false, -- last release is way too old and doesn't work on Windows
 		build = ":TSUpdate",
-		event = { "LazyFile", "VeryLazy" },
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		keys = {
-			{ "<c-space>", desc = "Increment Selection" },
-			{ "<bs>", desc = "Decrement Selection", mode = "x" },
+		event = { "LazyFile" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
 		},
-		opts_extend = { "ensure_installed" },
-		---@type TSConfig
-		---@diagnostic disable-next-line: missing-fields
 		opts = {
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 			indent = { enable = true },
+			autotag = { enable = true },
+
 			ensure_installed = {
 				"bash",
 				"c",
-        "c_sharp",
-        "css",
-				"diff",
+				"cpp",
+				"css",
 				"html",
 				"javascript",
-				"jsdoc",
 				"json",
 				"jsonc",
 				"lua",
-				"luadoc",
-				"luap",
 				"markdown",
 				"markdown_inline",
-				"printf",
 				"python",
 				"query",
 				"regex",
@@ -43,24 +38,31 @@ return {
 				"xml",
 				"yaml",
 			},
+
 			incremental_selection = {
 				enable = true,
 				keymaps = {
 					init_selection = "<C-space>",
 					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+					node_decremental = "<BS>",
 				},
 			},
+
 			textobjects = {
 				move = {
 					enable = true,
+					set_jumps = true,
+
 					goto_next_start = {
 						["]f"] = "@function.outer",
 						["]c"] = "@class.outer",
 						["]a"] = "@parameter.inner",
 					},
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]A"] = "@parameter.inner",
+					},
 					goto_previous_start = {
 						["[f"] = "@function.outer",
 						["[c"] = "@class.outer",
@@ -74,51 +76,10 @@ return {
 				},
 			},
 		},
+
 		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-			if type(opts.ensure_installed) == "table" then
-				opts.ensure_installed = SinkVim.dedup(opts.ensure_installed)
-			end
+      require("nvim-treesitter").install(opts.ensure_installed)
+      require("nvim-treesitter.config")
 		end,
-	},
-
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = "VeryLazy",
-		enabled = true,
-		config = function()
-			-- If treesitter is already loaded, we need to run config again for textobjects
-			-- if LazyVim.is_loaded("nvim-treesitter") then
-			-- local opts = LazyVim.opts("nvim-treesitter")
-			-- require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
-			-- end
-
-			-- When in diff mode, we want to use the default
-			-- vim text objects c & C instead of the treesitter ones.
-			local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-			local configs = require("nvim-treesitter.configs")
-			for name, fn in pairs(move) do
-				if name:find("goto") == 1 then
-					move[name] = function(q, ...)
-						if vim.wo.diff then
-							local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-							for key, query in pairs(config or {}) do
-								if q == query and key:find("[%]%[][cC]") then
-									vim.cmd("normal! " .. key)
-									return
-								end
-							end
-						end
-						return fn(q, ...)
-					end
-				end
-			end
-		end,
-	},
-
-	{
-		"windwp/nvim-ts-autotag",
-		event = "LazyFile",
-		opts = {},
 	},
 }

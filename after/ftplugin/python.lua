50cf050740f4204db379437553292921a8ebefd0
MerlinSink <MerlinSinkZero@outlook.com>
2026-02-10 19:38:49 +0800

    feat: add new plugin

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,9 @@
+local venv_slector_opts = {
+	options = {
+		notify_user_on_venv_activation = true,
+	},
+}
+SinkVim.keymap("n", "<leader>cv", "<CMD>:VenvSelect<CR>", "Select VirtualEnv")
+
+require("venv-selector").setup(venv_slector_opts)
 require("plugins.dap")
